a25e4998a50eb61594ec759d169440cd2ba50477
Eduardo Marques <eduardomar1703@gmail.com>
2025-06-06 18:53:50 -0300

    feat: create user repository

---
+++ internal/user/repository.go
@@ -0,0 +1,51 @@
+package user
+
+import (
+	"context"
+
+	"github.com/EduardoMark/my-finance-api/internal/db"
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	Create(ctx context.Context, arg db.CreateUserParams) error
+	GetUser(ctx context.Context, id uuid.UUID) (*db.User, error)
+	GetAllUser(ctx context.Context) ([]db.User, error)
+	Update(ctx context.Context, arg db.UpdateUserParams) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type userRepository struct {
+	db db.Queries
+}
+
+func NewUserRepository(db db.Queries) Repository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (r *userRepository) Create(ctx context.Context, arg db.CreateUserParams) error {
+	return r.db.CreateUser(ctx, arg)
+}
+
+func (r *userRepository) GetUser(ctx context.Context, id uuid.UUID) (*db.User, error) {
+	user, err := r.db.GetUser(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) GetAllUser(ctx context.Context) ([]db.User, error) {
+	return r.db.GetAllUsers(ctx)
+}
+
+func (r *userRepository) Update(ctx context.Context, arg db.UpdateUserParams) error {
+	return r.db.UpdateUser(ctx, arg)
+}
+
+func (r *userRepository) Delete(ctx context.Context, id uuid.UUID) error {
+	return r.db.DeleteUser(ctx, id)
+}
